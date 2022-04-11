6293548d7cb1cc9ac01013ab8ea45e1e3d399c7a
Jorge Romero Lagunas <jorchrl@Jorges-MacBook-Pro.local>
2022-04-11 00:38:54 -0500

    Add StateInfo type on State.ts on fsm module

--- modules/pokeFSM/States.ts
+++ modules/pokeFSM/States.ts
@@ -1,19 +1,18 @@
-type TaggedState<S extends string> = { type: S };
+type TaggedState<S extends string> = { type: S } & StateInfo;
+
+type StateInfo = {
+  mapId: number | null;
+  pkmId: number | null;
+  sectionId: number | null;
+};
 
 // States
 export type Search = TaggedState<"Search">;
 export type MapToSearch = TaggedState<"MapToSearch">;
-export type MapScreen = TaggedState<"MapScreen"> & {
-  mapId: number;
-};
-export type PkmInfoScreen = TaggedState<"PkmInfoScreen"> & {
-  pkmId: number;
-};
+export type MapScreen = TaggedState<"MapScreen">;
+export type PkmInfoScreen = TaggedState<"PkmInfoScreen">;
 export type PkmInfoToSearch = TaggedState<"PkmInfoToSearch">;
-export type MapSectionScreen = TaggedState<"MapSectionScreen"> & {
-  mapId: number;
-  sectionId: number;
-};
+export type MapSectionScreen = TaggedState<"MapSectionScreen">;
 
 // State type
 export type State =
